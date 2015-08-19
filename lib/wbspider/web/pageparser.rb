ca3f2acc08fd67e97e38e8c90bd3da1365e919f9
jiyaping <jiyaping0802@gmail.com>
2015-08-19 22:54:47 +0800

    test web page

--- lib/wbspider/web/pageparser.rb
+++ lib/wbspider/web/pageparser.rb
@@ -6,33 +6,33 @@ module Wbspider
       fields = {}
 
       is_repost = node.children.size == 2
-      if(not is_repost)
-        return ext_weibo_original(node.children[0])
-      end
+      return ext_weibo_original(node.children[0]) unless is_repost
 
       c_node = node.children[1]
       o_node = node.children[0]
 
       fields[:nickname], fields[:nick_href] = ext_nickname_href(o_node)
       fields[:userid] = user
-      fields[:weibo_id] = 
+      fields[:weibo_id] = ext_weibo_id(node)
       fields[:is_vip] = 'v' if ext_vip(o_node)
-      fields[:is_donate] = 'm' if ext__donate(o_node)
-      fields[:content] = ext_content(c_node)
+      fields[:is_donate] = 'm' if ext_donate(o_node)
+      fields[:content] = ext_original_content(c_node)
       fields[:content_pic] = ext_content_pic(c_node)
-      fields[:attitude], fields[:attitude_href] = ext_attitude(c_node)
-      fields[:report], fields[:report_href] = ext_report(c_node)
-      fields[:comment], fields[:comment] = ext_comment(c_node)
+      fields[:attitude_href], fields[:attitude] = ext_attitude(c_node)
+      fields[:report_href], fields[:report] = ext_repost(c_node)
+      fields[:comment_href], fields[:comment] = ext_comment(c_node)
       fields[:favorite_href] = ext_favorite(c_node)
+      fields[:generate_by] = ext_generate_by(node)
+      fields[:generate_time] = ext_generate_time(node)
 
-      fields[:original_nickname], fields[:original_nickname_href] = ext_original_nickname(o_node)
+      fields[:original_nickname_href], fields[:original_nickname] = ext_original_nickname(o_node)
       fields[:original_is_vip] = 'v' if ext_vip(o_node)
-      fields[:original_is_donate] = 'm' if ext__donate(o_node)
+      fields[:original_is_donate] = 'm' if ext_donate(o_node)
       fields[:original_content] = o_node.at('.ctt').text
       fields[:original_pic] = ext_content_pic(o_node)
       fields[:original_attitude] = ext_original_attitude(o_node)
       fields[:original_repost] = ext_original_repost(o_node)
-      fields[:original_comment] = ext_comment(o_node)
+      fields[:original_comment_href], fields[:original_comment] = ext_comment(o_node)
 
       fields
     end
@@ -41,20 +41,23 @@ module Wbspider
       fields = {}
 
       fields[:nickname], fields[:nick_href] = ext_nickname_href(node)
+      fields[:weibo_id] = ext_weibo_id(node)
       fields[:is_vip] = 'v' if ext_vip(node)
-      fields[:is_donate] = 'm' if ext__donate(node)
+      fields[:is_donate] = 'm' if ext_donate(node)
       fields[:content] = ext_content(node)
-      fields[:content_pic] = ext_content(node)
-      fields[:attitude], fields[:attitude_href] = ext_attitude(node)
-      fields[:report], fields[:report_href] = ext_report(node)
-      fields[:comment], fields[:comment] = ext_comment(node)
+      fields[:content_pic] = ext_content_pic(node)
+      fields[:attitude_href], fields[:attitude] = ext_attitude(node)
+      fields[:report_href], fields[:report] = ext_repost(node)
+      fields[:comment_href], fields[:comment] = ext_comment(node)
       fields[:favorite_href] = ext_favorite(node)
+      fields[:generate_by] = ext_generate_by(node)
+      fields[:generate_time] = ext_generate_time(node)
 
       fields
     end
 
     def ext_original_repost(o_node)
-      value = node.to_html.scan(/>转发\[(\d*)\]</).first
+      value = o_node.to_html.scan(/>转发\[(\d*)\]</).first
 
       return value.first if not value.nil?
 
@@ -69,10 +72,14 @@ module Wbspider
       0
     end
 
+    def ext_weibo_id(node)
+      $1 if node.attributes["id"].value.match /^M_(.*)/
+    end
+
     def ext_original_nickname(node)
       element = node.at('.cmt a')
 
-      return element.attributes[:href], element.text
+      return element.attributes['href'].value, element.text
     end
 
     def ext_page_time
@@ -84,11 +91,10 @@ module Wbspider
     end
 
     def ext_generate_time(node)
-      str = node.at("span[class='ct']").text.split("\u00A0").first
+      str = node.css("span[class='ct']").text.split("\u00A0").first
 
       str = str.sub('今天', DateTime.now.strftime('%m月%d日'))
-
-      if str.match(/(\d*)分钟之前/)
+      if str.match(/(\d*)分钟/)
         @pagetime + Rational($1.to_i, 1440)
       elsif str =~ /\d{2}:\d{2}/
         DateTime.strptime(str, '%m月%d日 %H:%M')
@@ -102,15 +108,15 @@ module Wbspider
     end
 
     def ext_favorite(node)
-      ext_weibo_status(node, 'fav')
+      ext_weibo_status(node, 'fav')[0]
     end
 
     def ext_comment(node)
       ext_weibo_status(node, 'comment')      
     end
 
-    def ext_report(node)
-      ext_weibo_status(node, 'report')
+    def ext_repost(node)
+      ext_weibo_status(node, 'repost')
     end
 
     def ext_attitude(node)
@@ -119,16 +125,30 @@ module Wbspider
 
     def ext_weibo_status(node, type)
       element = node.at("a[href*='#{type}']")
+      return unless element
 
-      num_arr = element.text.scan(/[(\d*)]/)
-      num = num_arr.first[0] if num_arr.size > 0
-      return element.attitudes[:href], num
+      num = 0
+      num = $1 if element.text.match(/\[(\d+)\]/)
+      
+      return element.attributes['href'].value, num
     end
 
     def ext_content_pic(node)
-      urls = node.to_html.scan(/[<a href="(.*)">]/)
+      urls = node.to_html.scan(/\[<a href="(.*)">\]/)
 
       urls.join(",")
+    end
+
+    def ext_original_content(node)
+      content = ''
+      node.children.each do |item|
+        next if item.to_html.match /class="cmt"/
+        break if item.to_html.match /(\/attitude\/)|(\/repost\/)|(\/comment\/)/
+
+        content << item.to_html
+      end
+
+      content
     end
 
     def ext_content(node)
@@ -149,15 +169,16 @@ module Wbspider
     end
 
     def ext_vip(node)
-      not node.search("img[alt='V']").nil?
+      not node.at("img[alt='V']").nil?
     end
 
-    def ext__donate(node)
-      not node.search("img[alt='M']").nil?
+    def ext_donate(node)
+      not node.at("img[alt='M']").nil?
     end
 
     def ext_nickname_href(node)
       element = node.at('.nk')
+      return unless element
 
       nickname = element.text
       nick_href = element.attributes['href'].value
--- test/test_pageparser.rb
+++ test/test_pageparser.rb
@@ -1,3 +1,5 @@
+#encoding : utf-8
+
 gem "minitest"
 require 'minitest/autorun'
 require 'wbspider'
@@ -7,11 +9,110 @@ class PageParser_Test < Minitest::Test
   include Wbspider::PageParser
 
   def setup
-      @page = Nokogiri::HTML(open('test/weibo.html'))
-      @nodes = @page.search("div[id*='M_']")
+    @original = Nokogiri::HTML(open('test/original_weibo.html'), nil, 'UTF-8').at("div[id*='M_']")
+    @repost = Nokogiri::HTML(open('test/repost_weibo.html'), nil, 'UTF-8').at("div[id*='M_']")
+    @timeline = Nokogiri::HTML(open('test/timeline_weibo.html'), nil, 'UTF-8').at("div[id*='M_']")
+    @timeline_repost = Nokogiri::HTML(open('test/timeline_repost.html'), nil, 'UTF-8').at("div[id*='M_']")
   end
 
-  def test_node_size
-    assert_equal  50, @nodes.size
+  def test_timeline_ext_nickname_href
+    assert_equal ['楚天都市报', 'http://weibo.cn/ctdsw?vt=4'], ext_nickname_href(@timeline)
+  end
+
+  def test_timeline_ext_donate
+    refute ext_donate(@timeline)
+  end
+
+  def test_timeline_ext_vip
+    assert ext_vip(@timeline)
+  end
+
+  def test_timeline_ext_content
+    assert ext_content(@timeline)
+  end
+
+  def test_timeline_ext_content_pic
+    assert_equal '', ext_content_pic(@timeline)
+  end
+
+  def test_timeline_ext_favorite
+    assert_equal 'http://weibo.cn/fav/addFav/Cwz9EFj9m?rl=0&vt=4&st=6956bb',
+                  ext_favorite(@timeline)
+  end
+
+  def test_timeline_ext_comment
+    assert_equal ['http://weibo.cn/comment/Cwz9EFj9m?uid=1720962692&rl=0&gid=10001&vt=4#cmtfrm', '3'],
+                 ext_comment(@timeline)
+  end
+
+  def test_timeline_ext_repost
+    assert_equal ['http://weibo.cn/repost/Cwz9EFj9m?uid=1720962692&rl=0&gid=10001&vt=4','2'],
+                  ext_repost(@timeline)
+  end
+
+  def test_timeline_ext_attitude
+    assert_equal ['http://weibo.cn/attitude/Cwz9EFj9m/add?uid=1741341313&rl=0&gid=10001&vt=4&st=6956bb','1'],
+                  ext_attitude(@timeline)
+  end
+
+  def test_timeline_ext_weibo_id
+    assert_equal "Cwz9EFj9m", ext_weibo_id(@timeline)
+  end
+
+  def test_timeline_ext_generate_by
+    assert_equal '来自微博 weibo.com', ext_generate_by(@timeline)
+  end
+
+  def test_timeline_ext_generate_time_1
+    @pagetime = DateTime.now
+    @node = Nokogiri::HTML::DocumentFragment.parse <<-EOHTML
+    <span class="ct">今天 16:07&nbsp;来自微博 weibo.com</span>
+  EOHTML
+    
+    assert_equal "#{@pagetime.strftime('%m月%d日')} 16:07",
+                  ext_generate_time(@node).strftime('%m月%d日 %H:%M')
+  end
+
+  def test_timeline_ext_generate_time_2
+    @pagetime = DateTime.now
+    @node = Nokogiri::HTML::DocumentFragment.parse <<-EOHTML
+    <span class="ct">08月18日 15:02&nbsp;来自微博 weibo.com</span>
+  EOHTML
+
+    assert_equal "08月18日 15:02",
+                  ext_generate_time(@node).strftime('%m月%d日 %H:%M')
+  end
+
+  def test_timeline_ext_generate_time_3
+    @pagetime = DateTime.now
+    @node = Nokogiri::HTML::DocumentFragment.parse <<-EOHTML
+    <span class="ct">10分钟前&nbsp;来自微博 weibo.com</span>
+  EOHTML
+    
+    assert_equal (@pagetime + Rational(10, 1440)).strftime('%m月%d日 %H:%M'),
+                  ext_generate_time(@node).strftime('%m月%d日 %H:%M')
+  end
+
+  def test_ext_weibo_original
+    assert ext_weibo_original(@timeline)
+  end
+
+  def test_repost_ext_weibo_content
+    c_node = @repost.children[1]
+
+    assert ext_original_content(c_node)
+  end 
+
+  def test_ext_weibo_repost
+    assert ext_single_weibo(@repost)
+  end
+
+  def test_ext_timeline_repost
+    @pagetime = DateTime.now
+    assert ext_single_weibo(@timeline_repost)
+  end
+
+  def user
+    "testttttttttt"
   end
 end
